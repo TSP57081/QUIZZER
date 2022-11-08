f20742d76d772920d8d63c67fda3011596634a2d
TIJIL <tsp57081@gmail.com>
2022-11-08 20:17:52 +0530

    UI Update

--- app/src/main/java/com/example/quizzer/MainActivity.kt
+++ app/src/main/java/com/example/quizzer/MainActivity.kt
@@ -9,25 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var greeting = findViewById<TextView>(R.id.greeting)
-        var sun_or_moon = findViewById<TextView>(R.id.sun_or_moon)
-
-        val date = Date()
-        val hour = date.hours
-        var unicode = 0x2600
-        var text : String = resources.getText(R.string.morning) as String
-        if (hour <= 6 || hour > 19) {
-            unicode = 0x1F31B
-        }
-        sun_or_moon.text = String(Character.toChars(unicode))
-
-        if (hour > 12) {
-            text = resources.getText(R.string.afternoon) as String
-            if (hour >= 17) {
-                text = resources.getText(R.string.evening) as String
-            }
-        }
-        greeting.text = text
     }
 }
